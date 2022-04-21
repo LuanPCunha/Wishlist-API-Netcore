2bd7fed5d36856fcd5d654c9f3a0bf97bbae12e8
Luan Pereira Cunha <luan.pereiracunha@gmail.com>
2022-04-20 23:44:01 -0300

    chore: Configurações iniciais

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Wishlist-API-Netcore
 Desafio Técnico (Wishlist) - LuizaLabs (4)
+
+
+**Migrations
+    0- trocar o projeto padrão para o Wishlist.Infrastructure
+    1- Add-Migration InitialDatabase
+    2- Update-Database
---
+++ Wishlist.API/Wishlist.API/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+using Wishlist.Domain.Product;
+using Wishlist.Domain.User;
+using Wishlist.Infrastructure.Persistence.Core;
+using Wishlist.Infrastructure.Persistence.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Wishlist-API", new OpenApiSecurityScheme
+    {
+        Description = "Use : (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddDbContext<Context>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("ProductDb")
+                                            , b => b.MigrationsAssembly("Wishlist.Infrastructure")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
